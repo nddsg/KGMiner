84adc7f5f80023765de24852c4caf25e503f5a43
bxshi <baoxu.shi@gmail.com>
2015-06-12 18:50:53 -0400

    separate project

---
+++ src/gbserver/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Baoxu Shi on 6/12/15.
+//
+
+#include "opts.h"
+
+#include <iostream>
+
+
+int main(int argc, const char *argv[]) {
+
+  opts options;
+  options.parse(argc, argv);
+
+  return 0;
+}
---
+++ src/gbserver/opts.cpp
@@ -0,0 +1,62 @@
+//
+// Created by Baoxu Shi on 6/11/15.
+//
+
+#include "opts.h"
+
+#include <iostream>
+#include <string>
+
+  opts::opts() : desc("Allowed options") {
+
+    std::string port_desc = "Port number, default is " + std::to_string(port);
+
+    desc.add_options()
+        ("help,h", "display help message")
+        ("edge,e", boost::program_options::value<std::string>()->required(),
+         "EdgeList filepath")
+        ("node,n", boost::program_options::value<std::string>()->required(),
+         "NodeList filepath")
+        ("type,t", boost::program_options::value<std::string>()->required(),
+         "EdgeTypeList filepath")
+        ("port,p", boost::program_options::value<int>(),
+         port_desc.c_str());
+  }
+
+  bool opts::parse(int argc, const char *argv[]) {
+    try {
+      boost::program_options::variables_map vm;
+      boost::program_options::store(boost::program_options::command_line_parser(argc, argv)
+                                        .options(opts::desc).run(), vm);
+
+      if (vm.count("help")) {
+        desc.print(std::cout);
+        return false;
+      }
+
+      /* Throw exception if missing important options */
+      boost::program_options::notify(vm);
+
+      if (vm.count("edge")) {
+        edge_filepath = vm["edge"].as<std::string>();
+      }
+      if (vm.count("node")) {
+        node_filepath = vm["node"].as<std::string>();
+      }
+      if (vm.count("type")) {
+        type_filepath = vm["type"].as<std::string>();
+      }
+      if (vm.count("port")) {
+        port = vm["port"].as<int>();
+      }
+    } catch (std::exception& err) {
+      std::cout << err.what() << std::endl;
+      return false;
+    }
+    catch(...) {
+      std::cout << "Unknown error" << std::endl;
+      return false;
+    }
+
+    return true;
+  }
