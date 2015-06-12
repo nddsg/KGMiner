398f5c1fa615d6222dff3f9b81382b59c361f904
bxshi <baoxu.shi@gmail.com>
2015-06-12 19:42:03 -0400

    load nodes

--- src/gbserver/main.cpp
+++ src/gbserver/main.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "opts.h"
+#include "node_loader.h"
 
 #include <iostream>
 
@@ -11,6 +12,10 @@ int main(int argc, const char *argv[]) {
 
   opts options;
   options.parse(argc, argv);
+
+  node_loader<std::string> nodes(options.getNode_filepath());
+
+  std::cout << "Load " << nodes.getMax_id() << " nodes\n";
 
   return 0;
 }
